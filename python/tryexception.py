0d6d73f326e7bbbe45049c893e7b918a84766aea
canonallan <133584930+canonallan@users.noreply.github.com>
2023-11-11 15:37:22 +0300

    Add files via upload

---
+++ python/tryexception.py
@@ -0,0 +1,30 @@
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    try:
+        result = num1 / num2
+        return result
+    except ZeroDivisionError:
+        return "Cannot divide by zero"
+
+if __name__ == "__main__":
+    try:
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+    except ValueError:
+        print("Invalid input. Please enter valid numbers.")
+    else:
+        try:
+            print(f"{num1} + {num2} = {add(num1, num2)}")
+            print(f"{num1} - {num2} = {subtract(num1, num2)}")
+            print(f"{num1} * {num2} = {multiply(num1, num2)}")
+            print(f"{num1} / {num2} = {divide(num1, num2)}")
+        except Exception as e:
+            print("An error occurred:", e)
